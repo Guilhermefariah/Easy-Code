b40d985759154c821a5952171e49ec7f79a29c9e
Guilherme Faria <guilhermefaria321@gmail.com>
2023-12-27 08:59:33 -0300

    js Ok

---
+++ _media/amountprice.js
@@ -0,0 +1,7 @@
+//contact
+function calc_total() {
+    let amountTot = parseInt(document.getElementById('cAmount').value);
+    amount = amountTot * 1500;
+    document.getElementById('cPrice').value = amount
+
+}
