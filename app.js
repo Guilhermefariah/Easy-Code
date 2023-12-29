62cc5440cf26cfe7660d6bccbd9d7eb82ef79ca2
Guilherme Faria <guilhermefaria321@gmail.com>
2023-12-28 21:00:25 -0300

    add function hours in javascript

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+function load(){
+let imageLoad = document.querySelector(".icon")
+let date = new Date()
+let hours = date.getHours()
+
+if (hours >= 0 && hours < 12) {
+    imageLoad.src= "image/1.jpg"
+    document.body.style.background = "linear-gradient(#ffffff, #87cefa)"
+
+}else if (hours >= 12 && hours < 18 ){
+    imamgeLoad.src = "image/2.jpg"
+    document.body.style.background = "linear-grient(#ffffff, #ffff00)"
+
+}else {
+    imageLoad.src = "image/3.jpg"
+    document.body.style.background = "linear-gradient(#ffffff 20%, #87cefa" 
+    }
+}
+
 function screenNew(screen){
     document.querySelector(".icon img").src = screen;
 }
+
 alert("Hello, Hoy is Day -" + Date());
+
