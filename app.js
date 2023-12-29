a8c7c9af84e6a54d22a6f15e2a7211608b680679
Guilherme Faria <guilhermefaria321@gmail.com>
2023-12-28 21:01:27 -0300

    Ok

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ let hours = date.getHours()
 
 if (hours >= 0 && hours < 12) {
     imageLoad.src= "image/1.jpg"
-    document.body.style.background = "linear-gradient(#ffffff, #87cefa)"
+    document.body.style.background = "linear-gradient(#ffffff 20%, #87cefa)"
 
 }else if (hours >= 12 && hours < 18 ){
     imamgeLoad.src = "image/2.jpg"
-    document.body.style.background = "linear-grient(#ffffff, #ffff00)"
+    document.body.style.background = "linear-grient(#ffffff 20%, #ffff00)"
 
 }else {
     imageLoad.src = "image/3.jpg"
