996966a37f497529c1199b5b8d516aee2a5f1d28
Guilhermefariah <guilhermefaria321@gmail.com>
2024-03-31 17:29:02 -0300

    CONTACT
    iniciando formulario atualizado email, name, passaword,

--- _media/amountprice.js
+++ _media/amountprice.js
@@ -3,5 +3,4 @@ function calc_total() {
     let amountTot = parseInt(document.getElementById('cAmount').value);
     amount = amountTot * 1500;
     document.getElementById('cPrice').value = amount
-
 }
