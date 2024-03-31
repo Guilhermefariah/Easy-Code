4b14d7dc9aa876d0a9690df12996d7d30a4ebf0a
Guilhermefariah <guilhermefaria321@gmail.com>
2024-03-31 11:46:47 -0300

    INDEX
    corrigido figcaption adicionando a variavel

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-function resetOpacity() {
-    var figcaption = document.getElementById('figcaption');
-    figcaption.classList.remove('show');
+function resetOpacity(figcaptionId) {
+    var figcaption = document.getElementById(figcaptionId);
+    figcaption.classList.remove('show');      
     setTimeout(function() {
         figcaption.classList.add('show');
     }, 100);
 }
+
 function screenNew(screen){
     document.querySelector(".container img").src = screen;
 }
