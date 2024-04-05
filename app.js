520706a819f5a1a5c8a186fd34a5de89890d4ffb
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-05 17:03:32 -0300

    INDEX
    definindo padrao index

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function resetOpacity(figcaptionId) {
-    var figcaption = document.getElementById(figcaptionId);
-    figcaption.classList.remove('show');      
-    setTimeout(function() {
-        figcaption.classList.add('show');
-    }, 100);
-}
+// function resetOpacity(figcaptionId) {
+//     var figcaption = document.getElementById(figcaptionId);
+//     figcaption.classList.remove('show');      
+//     setTimeout(function() {
+//         figcaption.classList.add('show');
+//     }, 100);
+// }
 
-function screenNew(screen){
-    document.querySelector(".container img").src = screen;
-}
+// // function screenNew(screen){
+// //     document.querySelector(".container img").src = screen;
+// // }
 
 
 
