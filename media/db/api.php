f70cfcce96df3e66cf76616b92086b4eaaab253a
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-05 10:33:53 -0300

    api
    passando para camelCase

--- media/db/api.php
+++ media/db/api.php
@@ -6,12 +6,12 @@ if ($_SERVER[$_SERVER] == "POST") {
 
     if (isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["senha"])) {
         if (isset($_POST["id"])) {
-            atualizar_usuario($conn, $_POST["id"], $_POST["nome"], $_POST["email"], $_POST["senha"]);
+            atualizarUsuario($conn, $_POST["id"], $_POST["nome"], $_POST["email"], $_POST["senha"]);
         } else {
-            criar_usuario($conn, $_POST["nome"], $_POST["email"], $_POST["senha"]);
+            criarUsuario($conn, $_POST["nome"], $_POST["email"], $_POST["senha"]);
         }
     } elseif (isset($_POST["id"])) {
-        apagar_usuario($conn, $_POST["id"]);
+        apagarUsuario($conn, $_POST["id"]);
     }
 }
 
