4e9331a0d8cf99bcad066a6a75d8e05aec5c9486
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-04 22:34:25 -0300

    api
    adicionando funcao criar usuario

--- media/db/api.php
+++ media/db/api.php
@@ -1,7 +1,13 @@
 <?php
 
+
 if ($_SERVER[$_SERVER] == "POST") {
     if (isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["senha"])) {
+        if (isset($_POST["id"])){
+            atualizar_usuario($conn, $_POST["id"], $_POST["nome"], $_POST["email"], $_POST["senha"]);
+        }else {
+            criar_usuario($conn, $_POST["nome"], $_POST["email"], $_POST["senha"]);
+        }
         
     }
 }
