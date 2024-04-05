2bbeb69be596bb9a3ec7f205794191b5ba0f97df
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-05 19:59:57 -0300

    app
    funcao de exibicao de pesquisa

---
+++ app.js
@@ -0,0 +1,13 @@
+function exbirPesquisa(results) {
+    const containerPesquisa = document.querySelector("#search-results");
+    containerPesquisa.innerHTML = "";
+
+    if (results.length === 0) {
+        containerPesquisa.innerHTML = "<p>Nenhum Resultado!</p> ";
+    } else {
+        results.forEach(function (result) {
+            const resultItem = document.createElement("div");
+            resultItem.textContent = result.textContent;
+        });
+    }
+}
