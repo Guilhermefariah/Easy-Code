19321f843b065ddc38de4a16d7eec7d18036744e
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-04 23:57:37 -0300

    funcoes
    adicionando criar e atualizar

--- media/db/funcoes.php
+++ media/db/funcoes.php
@@ -2,3 +2,20 @@
 
 require_once "../db/db.php";
 
+function acoes_usuarios($conn, $id) {
+    return acoes_usuariosdb($conn, $id);
+}
+function usuarios($conn){
+    return usuariosdb($conn);
+}
+function criar_usuario($conn, $nome, $email, $senha){
+    $criar_usuariodb = criar_usuariodb($conn, $nome, $email, $senha);
+    $mensagem = $criar_usuariodb == 1 ? "Criado com sucesso!" : "ERROR";
+    return api("../db/api.php?mensagem=$mensagem");
+}
+function atualizar_usuario($conn, $nome, $email, $senha){
+    $atualizar_usariodb($conn, $nome, $email, $senha);
+    $mensagem = $atualizar_usariodb == 1 ? "Atualizado  com sucesso!" : "Eroor";
+    return api("../db/api.php?mensagem=$mensagem");
+}
+
