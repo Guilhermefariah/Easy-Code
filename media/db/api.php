ed4748309e8718b1701c2982eba3796ce2a14541
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-04 23:15:05 -0300

    funcoes
    criando path fucoes para adicionar as funcoes do crud

--- media/db/api.php
+++ media/db/api.php
@@ -1,15 +1,20 @@
 <?php
 
+require_once "../db/funcoes.php";
 
 if ($_SERVER[$_SERVER] == "POST") {
+
     if (isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["senha"])) {
-        if (isset($_POST["id"])){
+        if (isset($_POST["id"])) {
             atualizar_usuario($conn, $_POST["id"], $_POST["nome"], $_POST["email"], $_POST["senha"]);
-        }else {
+        } else {
             criar_usuario($conn, $_POST["nome"], $_POST["email"], $_POST["senha"]);
         }
-    } else (isset($_POST["id"])){
+    } elseif (isset($_POST["id"])) {
         apagar_usuario($conn, $_POST["id"]);
     }
 }
+
+$users = usuarios($conn);
+
 
