6d25f9d14f15b70a2f35cb302bd9c8d9f342e0e3
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-06 17:31:59 -0300

    app
    adicionado array com as funcoes

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const linguagensDeProgramacao = [
+const dados = [
+    {
+        titulo: "Algoritmos", 
+        descricao: [
+            "Procedimentos bem definidos",
+            "Descrição 2 para Algoritmos.",
+            "Descrição 3 para Algoritmos.",
+            "Descrição 4 para Algoritmos.",
+            "Descrição 5 para Algoritmos."
+        ]
+    },
     {
         titulo: "JavaScript",
         descricao: "JavaScript é uma linguagem de programação de alto nível, interpretada e orientada a objetos."
@@ -40,3 +50,46 @@ const linguagensDeProgramacao = [
         descricao: "PHP é uma linguagem de script open-source amplamente usada, especialmente adequada para o desenvolvimento web."
     }
 ];
+const linguagensContainer = document.querySelector("#card-container");
+const buscarButton = document.querySelector("#searchInput");
+const mostrarDados = dados => {
+    linguagensContainer.innerHTML = "";
+    let html = '';
+    dados.forEach((e, index) => {
+        const collapseId = `collapse-${index}`;
+        const buttonId = `button-${index}`;
+        let descricaoHtml = Array.isArray(e.descricao) ? e.descricao.map(desc => `<a href="#" class="link-light">${desc}</a>`).join("<br>") : `<a href="#" class="link-light">${e.descricao}</a>`; 
+        html += `
+        <div class="col-4">
+            <div class="custom-icon-container position-relative">
+                <img class="image img-fluid border border-1 rounded-1"
+                    src="https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto&cs=tinysrgb&w=1260&h=427&dpr=1">
+            </div>
+            <div class="accordion mt-1">
+                <p>
+                    <button id="${buttonId}" class="btn btn-dark" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#${collapseId}" aria-expanded="false" aria-controls="collapse">
+                        ${e.titulo}
+                    </button>
+                </p>
+                <div style="min-height: 120px;">
+                    <div class="collapse" id="${collapseId}">
+                        <div class="card card-body bg-dark" style="width: 100%;">
+                            ${descricaoHtml}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    });
+    linguagensContainer.innerHTML = `
+    <div class="row">${html}</div>`;
+};
+
+
+buscarButton.addEventListener("keyup", (e) => {
+    const busca = dados.filter(i => i.titulo.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()));
+    mostrarDados(busca);
+})
+window.addEventListener("load", mostrarDados.bind(null,dados));
