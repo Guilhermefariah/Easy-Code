181522f36c47f10c8cb1fbf21368d87b28baacc6
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-06 13:39:45 -0300

    app
    adicionando array para buscar

---
+++ app.js
@@ -0,0 +1,42 @@
+const linguagensDeProgramacao = [
+    {
+        titulo: "JavaScript",
+        descricao: "JavaScript é uma linguagem de programação de alto nível, interpretada e orientada a objetos."
+    },
+    {
+        titulo: "Python",
+        descricao: "Python é uma linguagem de programação interpretada, de alto nível, multiparadigma e de tipagem dinâmica."
+    },
+    {
+        titulo: "Java",
+        descricao: "Java é uma linguagem de programação orientada a objetos e plataforma computacional."
+    },
+    {
+        titulo: "C++",
+        descricao: "C++ é uma linguagem de programação compilada, multi-paradigma, de alto nível e general-purpose."
+    },
+    {
+        titulo: "Ruby",
+        descricao: "Ruby é uma linguagem de programação interpretada, de alto nível e de uso geral."
+    },
+    {
+        titulo: "Swift",
+        descricao: "Swift é uma linguagem de programação desenvolvida pela Apple para o desenvolvimento de aplicativos iOS e macOS."
+    },
+    {
+        titulo: "C#",
+        descricao: "C# (C Sharp) é uma linguagem de programação orientada a objetos, desenvolvida pela Microsoft."
+    },
+    {
+        titulo: "Go",
+        descricao: "Go é uma linguagem de programação compilada, concorrente e de código aberto, desenvolvida pelo Google."
+    },
+    {
+        titulo: "Rust",
+        descricao: "Rust é uma linguagem de programação de sistema que é focada em segurança, concorrência e velocidade."
+    },
+    {
+        titulo: "PHP",
+        descricao: "PHP é uma linguagem de script open-source amplamente usada, especialmente adequada para o desenvolvimento web."
+    }
+];
