9ec0e12d5e147cec18101bb34eac9050cee8d37a
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-07 01:01:11 -0300

    framework
    adicionando frameworks

--- media/framework/frame.js
+++ media/framework/frame.js
@@ -1,6 +1,6 @@
-const dados = [
+const frameworks = [
     {
-        titulo: "Algoritmos", 
+        titulo: "AngularJS", 
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -11,7 +11,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Fluxogranma02.gif"
     },
     {
-        titulo: "JavaScript",
+        titulo: "Typescript",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -22,7 +22,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png"
     },
     {   
-        titulo: "HTML e CSS",
+        titulo: "Laravel",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -33,7 +33,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/CSS3_and_HTML5_logos_and_wordmarks.svg/1280px-CSS3_and_HTML5_logos_and_wordmarks.svg.png"
     },
     {
-        titulo: "Java",
+        titulo: "Spring Boot",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -44,7 +44,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png"
     },
     {
-        titulo: "C++",
+        titulo: "ASP.NET Core",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -55,7 +55,7 @@ const dados = [
         imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD4rstsLzUrHX2D9_-7PDwCKYGid5JWeOehQ_geDAYiw&s"
     },
     {
-        titulo: "Ruby",
+        titulo: "React",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -66,7 +66,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/800px-Ruby_logo.svg.png"
     },
     {
-        titulo: "Figma",
+        titulo: "Vue.js",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -77,7 +77,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg"
     },
     {
-        titulo: "C#",
+        titulo: "Django",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -88,7 +88,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/200px-C_Sharp_Logo_2023.svg.png"
     },
     {
-        titulo: "Go",
+        titulo: "Ruby on Rails",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -110,7 +110,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Rustacean-orig-noshadow.svg/220px-Rustacean-orig-noshadow.svg.png"
     },
     {
-        titulo: "PHP",
+        titulo: "Symfony",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -121,7 +121,7 @@ const dados = [
         imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Webysther_20160423_-_Elephpant.svg/200px-Webysther_20160423_-_Elephpant.svg.png"
     },   
     {
-        titulo: "Python",
+        titulo: "Tailwind",
         descricao: [
             "Procedimentos bem definidos",
             "Descrição 2 para Algoritmos.",
@@ -132,19 +132,19 @@ const dados = [
         imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyQlAY-m5UXpwyX4z78Vpi4WFnNiU7e311PwoZU3kyXQ&shttps://upload.wikimedia.org/wikipedia/pt/thumb/3/30/Java_programming_language_logo.svg/1200px-Java_programming_language_logo.svg.png"
     }
 ];
-const linguagensContainer = document.querySelector("#card-container");
+const linguagensContainer = document.querySelector("#framework-container");
 const buscarButton = document.querySelector("#searchInput");
-const mostrarDados = dados => {
+const mostrarFrameworks = frameworks => {
     linguagensContainer.innerHTML = "";
     let html = '';
-    dados.forEach((e, index) => {
+    frameworks.forEach((e, index) => {
         const collapseId = `collapse-${index}`;
         const buttonId = `button-${index}`;
         let descricaoHtml = Array.isArray(e.descricao) ? e.descricao.map(desc => `<a href="#" class="link-light">${desc}</a>`).join("<br>") : `<a href="#" class="link-light">${e.descricao}</a>`; 
         html += `
         <div class="col-4">
             <div class="custom-icon-container position-relative">
-                <img class="image img-fluid border border-1 rounded-5 align-top;"
+                <img class="image img-fluid border border-1 rounded-2 border-3 align-top;"
                 src="${e.imagem}" style="width: 100%; height: 250px;">
             </div>
             <div class="accordion mt-1">
@@ -169,7 +169,7 @@ const mostrarDados = dados => {
     <div class="row">${html}</div>`;
 };
 buscarButton.addEventListener("keyup", (e) => {
-    const busca = dados.filter(i => i.titulo.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()));
-    mostrarDados(busca);
+    const busca = frameworks.filter(i => i.titulo.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()));
+    mostrarFrameworks(busca);
 })
-window.addEventListener("load", mostrarDados.bind(null,dados));
+window.addEventListener("load", mostrarFrameworks.bind(null,frameworks));
