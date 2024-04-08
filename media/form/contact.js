8818252d73b52b04d865ed44c604ed6b4d739e73
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-04-07 23:33:01 -0300

    form"

--- media/form/contact.js
+++ media/form/contact.js
@@ -1,13 +1,13 @@
 document.getElementById('meuFormulario').addEventListener('submit', function(event) {
     event.preventDefault();
     let formDados = new FormData(this);
-    let reqXlm = new XMLHttpRequest();
-    reqXlm.open('POST', this.action, true);
-    reqXlm.onload = function() {
-        if (xhr.status === 200) {
+    let reqXml = new XMLHttpRequest();
+    reqXml.open('POST', this.action, true);
+    reqXml.onload = function() {
+        if (reqXml.status === 200) {
             window.location.href = "/index.html";
         } else {
         }
     };
-    xhr.send(formDados);
+    reqXml.send(formDados);
 });
